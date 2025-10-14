f59256e047dbd1f1ef6ba9dfcdfc026350e0205b
Anandhbasha <anadhakm@gmail.com>
2025-10-14 19:34:53 +0530

    datatypes

--- index.py
+++ index.py
@@ -1,9 +1,40 @@
-print("Hello Welcome to python",end="\t")
-print("This is first code of python")
+# print("hello Python")
+# print("Basics")
 
-# range
-# enter 4 digits and above
-# Input 4265
-# loop
-# output 17
-# 4 lines
+# print("apple","orange","kiwi",sep='-')
+# a=10
+# a=20
+# print(a)
+
+# datatypes
+# premitive
+# a=20  int
+# a=20.0  float
+# a="xyz" string
+# a=True  boolean
+# print(type(a))
+
+# = assigment operator
+# non premitive
+# complex data types
+# list
+# arr = [20,"abc",True,20.5] #index 0,1,2,3
+# # print(arr[3])
+# arr[0] = 15
+# print(arr[0])
+# # tuple
+# tuples = (20,60,80,90,90)
+# # tuples[0] = 32
+# print(tuples)
+
+# # set
+# sets = {22,88,44,88,44,55,60}
+# print(sets)
+# dictnory
+person = {
+    "name":"arun",
+    "age":20,
+    "city":"cbe"
+}
+
+print(person["name"], "\n" ,person["age"],'\n',person["city"])
